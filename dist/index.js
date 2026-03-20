2843e480a61e5401ed3d847bade15da975bfcf87
flojoho <hofkas.flo@gmx.at>
2026-03-20 20:46:55 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const svgImage = document.getElementById('svg-image');
+const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+const lineWidth = 10;
+svgPath.setAttributeNS(null, 'd', 'M 10 10 H 90 V 90 H 10 L 10 10');
+svgPath.setAttributeNS(null, 'stroke', 'white');
+svgPath.setAttributeNS(null, 'fill', 'none');
+svgPath.setAttributeNS(null, 'stroke-width', `${lineWidth}px`);
+svgPath.setAttributeNS(null, 'stroke-linecap', 'round');
+svgPath.setAttributeNS(null, 'stroke-linejoin', 'round');
+svgImage.appendChild(svgPath);
